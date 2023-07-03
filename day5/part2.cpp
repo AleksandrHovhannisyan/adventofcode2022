f5d858f8100ca86f5a333daf79e1921fd0f0e3e4
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2023-07-03 12:28:48 -0500

    Solve day5 part2

--- day5/part2.cpp
+++ day5/part2.cpp
@@ -5,9 +5,10 @@
 #include <string>
 #include <sstream>
 #include <numeric>
+#include <algorithm>
 #include "parser.h"
 
-// https://adventofcode.com/2022/day/5
+// https://adventofcode.com/2022/day/5#part2 
 
 int main() {
     std::ifstream inputFile;
@@ -16,14 +17,15 @@ int main() {
     auto moveInstructions = parseMoveInstructions(inputFile);
 
     for (auto instruction : moveInstructions) {
+        auto numCratesToMove = instruction.numCratesToMove;
         auto &sourceStack = crateStacks[instruction.sourceStackNumber - 1];
         auto &destinationStack = crateStacks[instruction.destinationStackNumber - 1];
 
-        for (auto i = 0; i < instruction.numCratesToMove; i++) {
-            auto crateToMove = sourceStack.front();
-            sourceStack.pop_front();
-            destinationStack.push_front(crateToMove);
-        }
+        // Move the desired number of elements as a group from sourceStack to destinationStack
+        destinationStack.insert(destinationStack.begin(), std::make_move_iterator(sourceStack.begin()), std::make_move_iterator(sourceStack.begin() + numCratesToMove));
+
+        // Erase the moved elements from sourceStack
+        sourceStack.erase(sourceStack.begin(), sourceStack.begin() + numCratesToMove);
     }
 
     std::string answer = std::accumulate(crateStacks.begin(), crateStacks.end(), std::string(),
@@ -35,3 +37,4 @@ int main() {
     inputFile.close();
     return 0;
 }
+
